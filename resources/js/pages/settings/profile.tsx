bff0155de9274311d63b6ac356bc28a1e590f5e8
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2026-02-01 12:55:17 +0800

    update settings

--- resources/js/pages/settings/profile.tsx
+++ resources/js/pages/settings/profile.tsx
@@ -143,7 +143,6 @@ export default function Profile({
                     </Form>
                 </div>
 
-                <DeleteUser />
             </SettingsLayout>
         </AppLayout>
     );
